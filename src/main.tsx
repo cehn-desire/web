47712196fbeaa192dbfa6202528f9e5efaea7c73
cehn-desire <13075289008@163.com>
2026-06-28 13:25:13 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-   <BrowserRouter basename="/web">
+  <BrowserRouter basename="/web">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
